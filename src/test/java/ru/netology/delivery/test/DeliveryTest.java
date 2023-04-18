4d194e52d4d1b72e2f7ffd159bcca08fdbfbc890
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-04-18 09:08:23 +0300

    finish

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -36,14 +36,14 @@ class DeliveryTest {
         $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
-        $(byText("Запланировать")).click();
+        $("button.button").click();
         $("[data-test-id=success-notification] .notification__title")
                 .shouldHave(exactText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=success-notification] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
-        $(byText("Запланировать")).click();
+        $("button.button").click();
         $("[data-test-id=replan-notification] .notification__content")
                 .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
